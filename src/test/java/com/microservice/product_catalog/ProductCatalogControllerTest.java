dc02bc4d22fee82bc81143a484c825fe3bc6c443
Soner <sonereren39@gmail.com>
2021-04-11 01:03:05 +0300

    mock mvc test implemented

--- src/test/java/com/microservice/product_catalog/ProductCatalogControllerTest.java
+++ src/test/java/com/microservice/product_catalog/ProductCatalogControllerTest.java
@@ -1,39 +1,73 @@
 package com.microservice.product_catalog;
 
-import com.microservice.product_catalog.dto.ProductDTO;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
-public class ProductCatalogControllerTest {
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ProductCatalogServiceTests {
 
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mvc;
 
     @Test
-    public void testCreate() throws Exception{
-
-        String exampleProductDTO = createProduct().toString();
-        RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .post("/product")
-                .accept(MediaType.APPLICATION_JSON).content(exampleProductDTO)
-                .contentType(MediaType.APPLICATION_JSON);
-
-        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
-
-        MockHttpServletResponse response = result.getResponse();
-        System.out.println(response.getStatus());
+    @Order(1)
+    public void testCreateProduct() throws Exception {
+        this.mvc.perform(MockMvcRequestBuilders.post("/product").contentType(MediaType.APPLICATION_JSON).content(getCreateProductData().toString()).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
     }
 
+    @Test
+    @Order(2)
+    public void testGetProductDetails() throws Exception {
+        String testProductId = getCreateProductData().getString("id");
+        this.mvc.perform(MockMvcRequestBuilders.get("/product/" + testProductId).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(content().json(getCreateProductData().toString()));
+    }
 
-    private ProductDTO createProduct(){
-        return ProductDTO.builder().id("1").desc("desc").title("title").imagePath("img").unitPrice(0.0).build();
+    @Test
+    @Order(3)
+    public void testGetProductList() throws Exception {
+        this.mvc.perform(MockMvcRequestBuilders.get("/product").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(content().json("[" + getCreateProductData().toString() + "]"));
+    }
+
+    @Test
+    @Order(4)
+    public void testUpdateProduct() throws Exception {
+        this.mvc.perform(MockMvcRequestBuilders.put("/product").contentType(MediaType.APPLICATION_JSON).content(getUpdateProductData().toString())).andExpect(status().isOk());
+    }
+
+    @Test
+    @Order(5)
+    public void testDeleteProduct() throws Exception {
+        String testProductId = getCreateProductData().getString("id");
+        this.mvc.perform(MockMvcRequestBuilders.delete("/product/" + testProductId)).andExpect(status().isOk());
+    }
+
+    //test data utility methods 
+    public JSONObject getCreateProductData() throws JSONException {
+        JSONObject createProductData = new JSONObject();
+        createProductData.put("id", "test-product-1");
+        createProductData.put("title", "test product 1");
+        return createProductData;
+    }
+
+    public JSONObject getUpdateProductData() throws JSONException {
+        JSONObject createProductData = new JSONObject();
+        createProductData.put("id", "test-product-1");
+        createProductData.put("title", "test product 1 updated");
+        return createProductData;
     }
 }
